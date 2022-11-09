16c15d033208735972356994116d61c831e7ea36
Vicoclico <victor.bonlieu@gmail.com>
2022-11-09 14:56:44 +0100

    Filtrage + quelque affichage modif

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Quizz;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\QuizzRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(QuizzRepository $quizzRepository): Response
+    #[Route('/', name: 'app_home', methods:['GET'])]
+    public function index(Request $request, QuizzRepository $quizzRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'quizzs' => $quizzRepository->findAll(),
+            'quizzs' => $quizzRepository->AddfiltersOnQuizz($request->query->all()),
+            'themes' => $quizzRepository->findAllTheme(),
+            'difficulties' => $quizzRepository->findAllDifficulty(),
         ]);
     }
 
--- src/Repository/QuizzRepository.php
+++ src/Repository/QuizzRepository.php
@@ -62,4 +62,46 @@ class QuizzRepository extends ServiceEntityRepository
            ->getOneOrNullResult()
        ;
    }
+   public function findAllTheme()
+   {
+        return $this->createQueryBuilder('q')
+            ->select('q.Theme')
+            ->distinct() 
+            ->orderBy('q.Theme','ASC')
+            ->getQuery()
+            ->getScalarResult()
+        ;
+   }
+   public function findAllDifficulty()
+   {
+        return $this->createQueryBuilder('q')
+            ->select('q.Difficulty')
+            ->distinct() 
+            ->orderBy('q.Difficulty','ASC')
+            ->getQuery()
+            ->getScalarResult()
+        ;
+   }
+   public function AddfiltersOnQuizz($filters): array
+   {
+        $qb = $this->createQueryBuilder('q');
+        if (array_key_exists('theme',$filters) && $filters['theme'] !== '') {
+            $qb->andWhere('q.Theme LIKE :th')
+            ->setParameter('th',$filters['theme'].'%');
+        }
+        if (array_key_exists('difficulty',$filters) && $filters['difficulty'] !== '') {
+            $qb->andWhere('q.Difficulty = :dif')
+            ->setParameter('dif',$filters['difficulty']);
+        }
+        if (array_key_exists('author',$filters) && $filters['author'] !== '') {
+            $qb->join('q.Author', 'u')
+            ->andWhere('u.Pseudo LIKE :author')
+            ->setParameter('author','%'.$filters['author'].'%');
+        }
+        if (array_key_exists('title',$filters) && $filters['title'] !== '') {
+            $qb->andWhere('q.Title LIKE :ti')
+            ->setParameter('ti','%'.$filters['title'].'%');
+        }
+        return $qb->getQuery()->getResult();
+    }
 }
